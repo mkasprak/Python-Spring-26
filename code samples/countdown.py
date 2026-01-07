6bb934cbef3c57c70de3fbe1d8b104250cdec653
Meri Kasprak <mkasprak@mchenry.edu>
2026-01-06 19:58:47 -0600

    advanced logic

---
+++ code samples/countdown.py
@@ -0,0 +1,16 @@
+# range(start, stop, step)
+for i in range(10, 0, -1):
+    print(f"{i}...")
+
+print("🚀 BLAST OFF!")
+print("      !")
+print("     / \\")
+print("    |   |")
+print("    |   |")
+print("   /|   |\\")
+print("  /_|___|_\\")
+print("    m m m")
+
+
+for day in ("Sunday", "Monday", "Tuesday"):
+    print(day)
